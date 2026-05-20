fd657ca620c3a73dca3efbe21264d0324020c436
umesh gupta <umesh.gupta112ec@gmail.com>
2026-05-20 19:34:44 +0530

    first commit

--- app.py
+++ app.py
@@ -1,108 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
-import os
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Add `enumerate` as a global function
-#app.jinja_env.globals.update(enumerate=enumerate)
-
-# Route for the homepage to display all registration
+# Home Route
 @app.route('/')
-def index():
-    conn = sqlite3.connect('registration.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM registration")
-    registration = c.fetchall()
-    
-    c.execute("select sum(registration_fees) FROM registration")
-    sums = c.fetchone()[0]
+def home():
+    return render_template('index.html')
 
-    c.execute("select count(*) FROM registration")
-    enrolcount = c.fetchone()[0]
+# Academics Route
+@app.route('/academics')
+def academics():
+    return render_template('academics.html')
 
-    c.execute("select count(*) FROM registration where class=10")
-    class10 = c.fetchone()[0]
-
-    c.execute("select count(*) FROM registration where class=11")
-    class11 = c.fetchone()[0]
-    
-    c.execute("select count(*) FROM registration where class=12")
-    class12 = c.fetchone()[0]
-
-    conn.close()
-   
-    return render_template("index.html", registration=registration, sums=sums, enrolcount=enrolcount, class10=class10, class11=class11, class12=class12)
-
-
-# Route for the homepage to display all registration
-@app.route('/feestructure')
-def fee_structure():
-   
-    return render_template("fee_structure.html")
-
-
-# Route for creating a new register
-@app.route('/new', methods=['GET', 'POST'])
-def new_register():
+# Contact Us Route
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
     if request.method == 'POST':
-        fname = request.form['fname']
-        lname = request.form['lname']
-        subject = request.form['subject']
-        classs = request.form['class']
-        title = request.form['title']
-        Regfee = request.form['Regfee']
-        comment = request.form['comment']
-        
-        conn = sqlite3.connect('registration.db')
-        c = conn.cursor()
-        c.execute("INSERT INTO registration (F_Name, L_Name, Subject, Class, Title, Registration_fees, comment) VALUES (?, ?, ?, ?, ?, ?, ?)", (fname, lname, subject, classs, title, Regfee, comment))
-        conn.commit()
-        conn.close()
-        return redirect(url_for('index'))
-    return render_template('new_register.html')
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        # Handle the contact form submission (save to database, send email, etc.)
+        return render_template('contact.html', success=True)
+    return render_template('contact.html')
 
-# Route for editing an existing register
-@app.route('/edit/<int:register_id>', methods=['GET', 'POST'])
-def edit_register(register_id):
-    conn = sqlite3.connect('registration.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM registration WHERE id = ?", (register_id,))
-    reg = c.fetchone()
+# Gallery Route
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
 
-    conn.close()
+# Teachers Pillars Route
+@app.route('/teachers')
+def teachers():
+    return render_template('teachers.html')
 
-    if request.method == 'POST':
-        fname = request.form['fname']
-        lname = request.form['lname']
-        subject = request.form['subject']
-        classs = request.form['class']
-        title = request.form['title']
-        regfee = request.form['regfee']
-        comment = request.form['comment']
-        conn = sqlite3.connect('registration.db')
-      
-        c = conn.cursor()
-        c.execute("UPDATE registration SET F_Name = ?, L_name = ?, Subject = ?, Class=?,Title=?,Registration_fees=?,comment=? WHERE id = ?", (fname, lname, subject, classs, title, regfee, comment, register_id))
-        conn.commit()
-        conn.close()
-        return redirect(url_for('index'))
-    return render_template('edit_register.html', reg=reg)
-
-# Route for deleting a register
-@app.route('/delete/<int:register_id>')
-def delete_register(register_id):
-    conn = sqlite3.connect('registration.db')
-    c = conn.cursor()
-    c.execute("DELETE FROM registration WHERE id = ?", (register_id,))
-    conn.commit()
-    conn.close()
-    return redirect(url_for('index'))
-
-
-
-if __name__ == "__main__":
-      app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 3000)))
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
